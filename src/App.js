f90d84808ce27ad9c96c5cc3801b5e7b3422a39c
kien242 <kien242tran@gmail.com>
2023-07-06 22:43:27 +0700

    Update App.js, index.js, and index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import Button from '@mui/material/Button';
-import { Box, Grid, IconButton, Stack } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send'
 import * as UserCard from './Component/UserCard';
-
+import * as RepoCard from './Component/RepoCard';
 
 function App() {
   return (
-    <UserCard.UserCard
-      user={{
-        name: "ZMK firmware",
-        twitter: "@johndoe",
-        avatar: "https://marmelab.com/posters/avatar-46.jpeg?size=32x32"
-      }}
-    />
+    <>
+      <UserCard.UserCard
+        user={{
+          name: "ZMK firmware",
+          twitter: "@johndoe",
+          avatar: "https://marmelab.com/posters/avatar-46.jpeg?size=32x32"
+        }}
+      />
+
+      <RepoCard.RepoCard
+        user={{
+          name: "ZMK firmware",
+          twitter: "@johndoe",
+          avatar: "https://marmelab.com/posters/avatar-46.jpeg?size=32x32"
+        }}
+      />
+    </>
+
   )
 }
 
--- src/Component/RepoCard/index.js
+++ src/Component/RepoCard/index.js
@@ -1,36 +1,57 @@
-import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
-import style from "./style.module.scss"
-import { Container } from 'react-bootstrap';
+import { Card, CardContent, Avatar, Box, Typography, IconButton, CardActionArea } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
-export const showRepo = (props) => {
-  return (
-    <>
-      <div className="card my-4" style={{ maxHeight: 150 }}>
-        <div className="row g-2 my-1">
-          <div className={`col-10 ${style.borderRight}`}>
-            <Container fluid>
-            <div className={`${style.centerBlock}`}>
-                <p className={`${style.p}`}>dkh</p>
-              </div>
-              <div>
-                <p className={`${style.p}`}>adfad</p>
-              </div>
-              <div>adklhb</div>
-            </Container>
-          </div>
-          <div className={`col-2`}>
-            <div className={`container`}>
-              <div className='row'>
-                <button type="button" className="btn btn-primary m-1">Clone</button>
-              </div>
-              <div className='row'>
-                <button type="button" className="btn btn-primary m-1">Clone with sub-module</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
+export const RepoCard = ({ user }) => (
+  <Card
+    sx={{
+      display: "inline-block",
+      bgcolor: "green", m: 1,
+      borderRadius: 4
+    }}>
+    <CardContent
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        pt: 2,
+        "&:last-child": { pb: 2 }
+      }}
+    >
+      <Avatar
+        variant="rounded"
+        src={user.avatar}
+        sx={{ width: 48, height: 48 }}
+      />
+
+      <CardActionArea>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center"
+          }}>
+          <Box px={3}>
+            <Typography variant="h6" sx={{ lineHeight: 1 }}>
+              {user.name}
+            </Typography>
+            <Typography variant="caption" color="textSecondary">
+              {user.twitter}
+            </Typography>
+          </Box>
+
+          <IconButton
+            size="small"
+            sx={{
+              bgcolor: "#fff",
+              boxShadow: "0 1px 4px 0 rgba(0,0,0,0.12)",
+              "&:hover": {
+                bgcolor: "#000",
+                color: "#fff"
+              }
+            }}
+          >
+            <AddIcon />
+          </IconButton>
+        </Box>
+      </CardActionArea>
+    </CardContent>
+  </Card>
+);
